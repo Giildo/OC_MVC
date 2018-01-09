72d87f7da46c2d2cc061247114aabddbf7f66915
Giildo <giildo.jm@gmail.com>
2018-01-09 11:31:22 +0100

    Création d'une gestion des erreurs

--- model/frontend.php
+++ model/frontend.php
@@ -38,13 +38,6 @@ function postComment($postId, $author, $comment)
 // Nouvelle fonction qui nous permet d'éviter de répéter du code
 function dbConnect()
 {
-    try
-    {
-        $db = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root');
-        return $db;
-    }
-    catch(Exception $e)
-    {
-        die('Erreur : '.$e->getMessage());
-    }
+    $db = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root');
+    return $db;
 }
