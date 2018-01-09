4e2da56b2a351159707de2edd84beb7c4a12d1ed
Giildo <giildo.jm@gmail.com>
2018-01-09 11:09:35 +0100

    Modifications "cosmétiques" : - les variables ont été écrites en anglais - les <?php echo ... ?> modifiés en <?= ... ?>

--- modele.php
+++ modele.php
@@ -1,17 +1,16 @@
 <?php
-function getBillets()
+function getPosts()
 {
-	try
-	{
-		$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root');
-	}
-	catch(Exception $e)
-	{
-		die('Erreur : '.$e->getMessage());
-	}
+    try
+    {
+        $db = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root');
+    }
+    catch(Exception $e)
+    {
+        die('Erreur : '.$e->getMessage());
+    }
 
-	$req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
+    $req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
 
-	return $req;
+    return $req;
 }
-?>
